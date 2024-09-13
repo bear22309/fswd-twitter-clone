4f28635b1205e9c92315ec69eb5e3bbedc0a3408
bear22309 <150596708+bear22309@users.noreply.github.com>
2024-09-13 10:14:30 -0400

    yarn, bundle and updated index.js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,22 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
+// Import React and ReactDOM
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+import { createRoot } from 'react-dom/client'; 
+
+const HelloWorld = () => <h1>Hello, World!</h1>;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const appDiv = document.createElement('div');
+  appDiv.id = 'app'; 
+  document.body.appendChild(appDiv);
+
+  
+  const root = createRoot(appDiv);
+  root.render(<HelloWorld />);
+});
+
+
+const channels = require.context('.', true, /_channel\.js$/);
+channels.keys().forEach(channels);
